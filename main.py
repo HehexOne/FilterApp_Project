fce21d8d2b3782aa978a8a79cc59c91639999352
HehexOne <werty.mn2@yandex.ru>
2018-12-18 21:14:51 +0300

    Created open function

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog, QMessageBox)
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog)
+from PyQt5.QtGui import QPixmap
 from filters import gotham, kelvin, lomo, nashville
 from ui import Ui_MainWindow
+from shutil import copy
 
 
 class Example(QMainWindow, Ui_MainWindow):
@@ -21,10 +25,19 @@ class Example(QMainWindow, Ui_MainWindow):
         self.lomo_button.clicked.connect(self.lomo_filter)
         self.nashville_button.clicked.connect(self.nashville_filter)
 
+    def reload_image(self):
+        pixmap = QPixmap(self.temp_image).scaled(self.image_label.size(), Qt.KeepAspectRatio)
+        self.image_label.setPixmap(pixmap)
+
+    def copy_to_tmp(self):
+        copy(self.image_path, "tmp/")
+        self.temp_image = "tmp/" + self.image_path.split("/")[-1]
+
     def open_image(self):
         filt = "Images (*.png *.jpg)"
         self.image_path, _ = QFileDialog.getOpenFileName(self, 'Open your image', filter=filt)
-
+        self.copy_to_tmp()
+        self.reload_image()
 
     def save_image(self):
         pass
